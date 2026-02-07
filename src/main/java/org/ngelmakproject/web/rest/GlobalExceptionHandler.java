e6e2e174b0b331ab49232daec06f222de893f0f2
Youssouph Faye <youssouph.faye@gmail.com>
2026-02-07 17:56:13 +0100

    Add exception handler

---
+++ src/main/java/org/ngelmakproject/web/rest/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.ngelmakproject.web.rest;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ngelmakproject.web.rest.errors.BadRequestAlertException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(BadRequestAlertException.class)
+	public ResponseEntity<Object> handleBadRequestAlertException(BadRequestAlertException ex) {
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("timestamp", Instant.now());
+		body.put("status", ex.getStatus().value());
+		body.put("error", ex.getStatus().getReasonPhrase());
+		body.put("message", ex.getMessage());
+		body.put("entity", ex.getEntityName());
+		body.put("errorKey", ex.getErrorKey());
+
+		return ResponseEntity
+				.status(ex.getStatus())
+				.body(body);
+	}
+}
